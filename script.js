b4b9421d42d68a3303c76af1681b1aa245ec582a
FKay <fatima_kharodia12@hotmail.com>
2022-10-26 13:08:42 -0700

    changed grid so size is changeable and updated css

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 //Sets important constants and variables
 
 const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+/* let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell"); */
+let cell;
+let target; 
 
+function makeCell (numCell) {
+    for (let c = 0; c < numCell; c++) {
+        cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
+}
 
-defaultGrid();
+function makeGrid (numRow, numCol) {
+    container.style.setProperty('--numRows', numRow); 
+    container.style.setProperty('--numCols', numCol); 
+    for (let r = 0; r < numRow; r++) {
+        makeCell(numCol);
+    }
+}
+
+function promptGrid() {
+    let number = prompt("Choose grid size between 5-64", 16); 
+     if (number >= 5 && number <= 64) {
+         makeGrid (number,number);
+     }else {
+         do {
+             number = prompt("Invalid size! Try Again! Make sure your value is between 5-64!");
+         }
+         while(number < 5 || number > 64);
+         makeGrid (number,number);
+     }
+ }
+
+ 
+promptGrid();
+
+/* defaultGrid();
 //Creates a default grid sized 16x16 
 function defaultGrid() {
     makeRows(16);
@@ -31,7 +64,7 @@ function makeColumns(cellNum) {
         };
 
     };
-};
+}; */
 
 document.querySelectorAll(".cell").forEach(item => {
     item.addEventListener('mouseover', event => {
@@ -42,7 +75,7 @@ document.querySelectorAll(".cell").forEach(item => {
 function reset()
 {
     container.textContent="";
-    defaultGrid();    
+    promptGrid();    
     document.querySelectorAll(".cell").forEach(item => {
         item.addEventListener('mouseover', event => {
             item.style.backgroundColor = "black";
